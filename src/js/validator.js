a61a30beee9bf83eb7e9254de49240b904b67820
max.iw15 <max.iw15@yandex.ru>
2023-10-05 14:29:11 +0300

    checkCardNumber checkCardSystem release

--- src/js/validator.js
+++
@@ -1,3 +0,0 @@
-export default function isValidInn(value) {
-  return true;
-}
--- src/js/widget.js
+++ src/js/widget.js
@@ -1,5 +1,3 @@
-import isValidInn from "./validator";
-
 export default class CardWidget {
   constructor(ParentEl) {
     this.parentEl = ParentEl;
@@ -10,12 +8,12 @@ export default class CardWidget {
     <div class="col-md-5">
         <h3>Check your credit card number</h3>
         <ul class="cards list-unstyled">
-            <li><span class="card visa" title="Visa">Visa</span></li>
-            <li><span class="card master" title="Mastercard">Mastercard</span></li>
-            <li><span class="card amex" title="American Express">American Express</span></li>
-            <li><span class="card discover" title="Discover">Discover</span></li>
-            <li><span class="card jcb" title="JCB">JCB</span></li>
-            <li><span class="card diners_club" title="Diners Club">Diners Club</span></li>
+            <li><span class="card visa cdisabled" title="Visa">Visa</span></li>
+            <li><span class="card master cdisabled" title="Mastercard">Mastercard</span></li>
+            <li><span class="card amex cdisabled" title="American Express">American Express</span></li>
+            <li><span class="card discover cdisabled" title="Discover">Discover</span></li>
+            <li><span class="card jcb cdisabled" title="JCB">JCB</span></li>
+            <li><span class="card mir cdisabled" title="Мир">Мир</span></li>
         </ul>
         <form id="form" class="form-inline">
         <div class="form-group">
@@ -25,10 +23,6 @@ export default class CardWidget {
         </form>
     </div>
         `;
-  }
-
-  static get submitSelector() {
-    return "#form";
   }
 
   static get inputSelector() {
@@ -41,17 +35,60 @@ export default class CardWidget {
 
   bindToDOM() {
     this.parentEl.innerHTML = CardWidget.markup;
-    //
     this.element = this.parentEl.querySelector(CardWidget.selector);
-    // this.submit = this.element.querySelector(CardWidget.submitSelector);
     this.input = this.element.querySelector(CardWidget.inputSelector);
-    console.log(this.element);
     this.element.addEventListener("submit", this.onSubmit);
+  }
+  checkCardNumber(value) {
+    if (value.length < 13) return false;
+    let sum = 0;
+    for (let i = 0; i < value.length; i++) {
+      let cardNumber = parseInt(value[i], 10);
+
+      if ((value.length - i) % 2 === 0) {
+        cardNumber *= 2;
+        if (cardNumber > 9) {
+          cardNumber -= 9;
+        }
+      }
+
+      sum += cardNumber;
+    }
+
+    return sum !== 0 && sum % 10 === 0;
+  }
+
+  checkCardSystem(value) {
+    if (/^4\d{12}(?:\d{3})/.test(value)) {
+      return ".visa";
+    } else if (/(^5[1-5])\d{14}/.test(value)) {
+      return ".master";
+    } else if (/(^3[47])\d{13}/.test(value)) {
+      return ".amex";
+    } else if (/^(?:2131|1800|35\d{3})\d{11}/.test(value)) {
+      return ".jcb";
+    } else if (/(^2||6)\d{15}/.test(value)) {
+      return ".mir";
+    } else if (
+      /^65[4-9][0-9]{13}|64[4-9][0-9]{13}|6011[0-9]{12}|(622(?:12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|9[01][0-9]|92[0-5])[0-9]{10})/.test(
+        value
+      )
+    ) {
+      return ".discover";
+    }
+
+    return null;
+  }
+
+  changeCardImage() {
+    return "";
   }
 
   onSubmit(e) {
     e.preventDefault();
     const value = this.input.value;
-    console.log(value);
+    if (this.checkCardNumber(value)) {
+      alert(this.checkCardSystem(value));
+    }
   }
 }
